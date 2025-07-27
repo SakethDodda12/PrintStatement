e48e1a1b844b702fe37ee22118c320b7725adc0d
SakethDodda12 <sakethdodda57@gmail.com>
2025-07-26 20:33:32 -0500

    Add files via upload

---
+++ prog2.py
@@ -0,0 +1,13 @@
+x = 5
+y = "saketh"
+print(x)
+print(y)
+
+codingal = "Hello World"
+print(codingal)
+
+name = input("enter your name")
+
+import keyword
+print("Python keywords are... \n")
+print(keyword.kwlist)
